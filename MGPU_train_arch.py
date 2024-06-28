fc51e9bea6bfc9237fba2981a82236a5a6a49988
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2024-06-28 17:58:55 -0400

    Add files via upload

--- MGPU_train_arch.py
+++ MGPU_train_arch.py
@@ -101,7 +101,12 @@ def main():
     elif args.dataset.lower() == 'stl10':
         fid_stat = 'fid_stat/stl10_train_unlabeled_fid_stats_48.npz'
     elif args.dataset.lower() == 'celeba':
-        fid_stat = 'fid_stat/fid_stats_celebA_train.npz'
+        if args.img_size == 64:
+            fid_stat = 'fid_stat/fid_stats_celeba64_train.npz'
+        elif args.img_size == 128:
+            fid_stat = 'fid_stat/fid_stats_celeba128_train.npz'
+        else:
+            raise NotImplementedError(f'no fid stat for {args.dataset.lower()} with size {args.img_size}')
     else:
         raise NotImplementedError(f'no fid stat for {args.dataset.lower()}')
     assert os.path.exists(fid_stat)
