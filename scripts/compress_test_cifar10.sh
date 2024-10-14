a4a3f2e4dd84e36381547ddc454d332ed1541d83
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2024-10-14 17:53:14 -0400

    Delete scripts directory

--- scripts/compress_test_cifar10.sh
+++
@@ -1,3 +0,0 @@
-
-# Compress Small Network
-python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset cifar10 --bottom_width 4 --img_size 32 --arch arch_cifar10 --draw_arch False --checkpoint Compress/cifar10_small_1by15_2024_05_05_02_38_59 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name Compress/test_cifar10_small  --byrank
--- scripts/compress_test_stl10.sh
+++
@@ -1,2 +0,0 @@
-# Train Small Network
-python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset stl10 --bottom_width 6 --img_size 48 --arch arch_cifar10 --draw_arch False --checkpoint Compress/stl10_small_1by20_2024_05_06_23_32_55 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name Compress/test_stl10_small  --byrank
