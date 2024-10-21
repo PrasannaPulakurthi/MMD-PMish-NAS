0f72d5df0e995523c51ed4f3576eab1df379d553
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2024-10-20 22:50:12 -0400

    Add files via upload

--- scripts/compress_test.sh
+++ scripts/compress_test.sh
@@ -1,7 +1,12 @@
 ## CIFAR10
-# Compress Small Network
-python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset cifar10 --bottom_width 4 --img_size 32 --arch arch_cifar10 --draw_arch False --checkpoint Compress/cifar10_small_1by15_2024_05_05_02_38_59 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name Compress/test_cifar10_small  --byrank
+python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset cifar10 --bottom_width 4 --img_size 32 --arch arch_cifar10 --draw_arch False --checkpoint compress/cifar10_small_1by15_2024_05_05_02_38_59 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name test/compress_cifar10_small --act pmishact --byrank
+
+## CIFAR100
+python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset cifar100 --bottom_width 4 --img_size 32 --arch arch_cifar10 --draw_arch False --checkpoint compress/cifar100_small_1by15_2024_10_19_02_24_51 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name test/compress_cifar100_small --act pmishact --byrank
 
 ## STL10
-# Train Small Network
-python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset stl10 --bottom_width 6 --img_size 48 --arch arch_cifar10 --draw_arch False --checkpoint Compress/stl10_small_1by20_2024_05_06_23_32_55 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name Compress/test_stl10_small  --byrank
+python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset stl10 --bottom_width 6 --img_size 48 --arch arch_cifar10 --draw_arch False --checkpoint compress/stl10_small_1by20_2024_05_06_23_32_55 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 50 --exp_name test/compress_stl10_small --act pmishact --byrank
+
+## CELEBA
+python MGPU_test_cpcompress.py --gpu_ids 0 --num_workers 1 --dataset celeba --bottom_width 8 --img_size 64 --arch arch_cifar10 --draw_arch False --checkpoint compress/celeba_small_1by20_2024_09_30_12_43_51 --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --num_eval_imgs 50000 --eval_batch_size 25 --exp_name test/compress_celeba_small --act pmishact --byrank
+
