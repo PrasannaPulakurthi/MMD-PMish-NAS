58fb7192ceb19a67dc23d9bc80dfbc0373ab01cd
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2024-10-23 01:12:06 -0400

    Update activation_functions.py

--- archs/activation_functions.py
+++ archs/activation_functions.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 # Define the Activation Function class
 class Activation(nn.Module): 
@@ -15,7 +14,7 @@ class Activation(nn.Module):
         elif act == 'mish':
             self.activation_fn = nn.Mish()
         elif act == 'pmishact':
-            self.activation_fn = PMish()
+            self.activation_fn = PMishActivation()
         else:
             raise NotImplementedError(f'No activation function found for {act}')
 		
@@ -32,13 +31,14 @@ class SwishActivation(nn.Module):
 	def forward(self, x): 
 		return x * self.sigmoid(self.beta*x)
     
-# Define the PMish activation function 
-class PMish(nn.Module):
-    def __init__(self):
-        super(PMish, self).__init__()
-	self.beta = nn.Parameter(torch.ones(1).type(torch.cuda.FloatTensor))
-
-    def forward(self, x):
-        return x * torch.tanh(F.softplus(self.beta * x) / self.beta)
-
+# Define the Mish activation function 
+class PMishActivation(nn.Module): 
+	def __init__(self): 
+		super(PMishActivation, self).__init__() 
+		self.beta = nn.Parameter(torch.ones(1).type(torch.cuda.FloatTensor))
+		self.tanh_fn = nn.Tanh()
+		self.softplus_fn = nn.Softplus()
+		
+	def forward(self, x): 
+		return x * self.tanh_fn((1/self.beta) * self.softplus_fn(self.beta*x)) 
 	
