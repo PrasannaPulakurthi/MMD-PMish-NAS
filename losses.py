ed7072d50cba38381bdf06b2b16d21d730fc747c
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2024-10-29 23:20:26 -0400

    Add files via upload

--- losses.py
+++ losses.py
@@ -46,17 +46,23 @@ class MMD_loss(nn.Module):
         return lossMMD2
       
 class Modified_MMD_loss(nn.Module):
-    def __init__(self, bu = 4, bl = 1/4):
+    def __init__(self, bu = 4, bl = 1/4, lambda_m=0.001):
       super(Modified_MMD_loss, self).__init__()
       self.fix_sigma = 1
       self.bl = bl
       self.bu = bu
+      self.lambda_m = lambda_m
       return
   
     def phi(self,x,y):
       total0 = x.unsqueeze(0).expand(int(x.size(0)), int(x.size(0)), int(x.size(1)))
       total1 = y.unsqueeze(1).expand(int(y.size(0)), int(y.size(0)), int(y.size(1)))
       return(((total0-total1)**2).sum(2))
+    
+    def phi_abs(self,x,y):
+      total0 = x.unsqueeze(0).expand(int(x.size(0)), int(x.size(0)), int(x.size(1)))
+      total1 = y.unsqueeze(1).expand(int(y.size(0)), int(y.size(0)), int(y.size(1)))
+      return((torch.abs(total0-total1)).sum(2))
         
     def forward(self, source, target, type):
       M = source.size(dim=0)
@@ -76,8 +82,10 @@ class Modified_MMD_loss(nn.Module):
         YY_l = torch.exp(-alpha*torch.max(L2_YY,bl))
         XX = (1/(m*(m-1))) * (torch.sum(XX_u) - torch.sum(torch.diagonal(XX_u, 0)))
         YY = (1/(m*(m-1))) * (torch.sum(YY_l) - torch.sum(torch.diagonal(YY_l, 0)))
-        YY_dist = torch.mean(torch.clamp(L2_YY,min=bl))
-        lossD = XX - YY + 0.001 * YY_dist
+        L2_YY = self.phi_abs(target, target)
+        L2_YY = torch.sqrt(torch.clamp(L2_YY,min=bl))
+        YY_dist = (1/(m*(m-1))) * (torch.sum(L2_YY) - torch.sum(torch.diagonal(L2_YY, 0)))
+        lossD = XX - YY + self.lambda_m * YY_dist
         return lossD
       elif type == "gen":
         XX_u = torch.exp(-alpha*L2_XX)
